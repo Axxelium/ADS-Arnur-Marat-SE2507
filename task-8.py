009a6af48ddac1858bdc959bee5805b7334c9ddb
Axxelium <arnurmarat8@gmail.com>
2026-03-30 02:22:02 +0500

    task 9

--- task-8.py
+++ task-8.py
@@ -2,7 +2,7 @@ def task8(n):
     if n == 0:
         return
     else:
-        print(n % 10, end=" ")
+        print(n % 10, end="")
         task8(n // 10)
 
 a = int(input())
---
+++ task-9.py
@@ -0,0 +1,10 @@
+def task9(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return task9(n - 1) + task9(n - 2)
+
+a = int(input())
+print(task9(a))
